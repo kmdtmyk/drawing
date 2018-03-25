c3e967ad3be12a3b04bb1ca215dd029327bae05f
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-03-25 11:37:35 +0900

    Add paginate statistic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def format_number(number)
+    number_with_delimiter number
+  end
+
+  def paginate_statistic(page)
+    render 'kaminari/statistic', page: page
+  end
+
 end
