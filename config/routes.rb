2b0407f0d6143ea58263dabfcbc0f037540b2fb8
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-03-25 13:17:12 +0900

    Remove route categories#show

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   resources :drawings
-  resources :categories
+  resources :categories, except: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
