53f0b925696cdab5c48a2baad1b228a26c45a018
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-03-27 00:47:47 +0900

    Change login page layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :drawings
   resources :categories, except: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
