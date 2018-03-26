942eac99fe89218b5323bd2de488e478d7fcdc44
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-03-27 00:53:05 +0900

    Disable user recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise(
     :database_authenticatable,
     # :registerable,
-    :recoverable,
+    # :recoverable,
     :rememberable,
     :trackable,
     :validatable
