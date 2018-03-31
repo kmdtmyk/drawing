34048209681fecf8cb7e1739a88c7a1957409340
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-03-31 18:31:39 +0900

    Change categories redirect path

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,7 +15,7 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to ({action: :index}), notice: 'Category was successfully created.'
+      redirect_to categories_url, notice: 'Category was successfully created.'
     else
       render :new
     end
@@ -23,7 +23,7 @@ class CategoriesController < ApplicationController
 
   def update
     if @category.update(category_params)
-      redirect_to ({action: :index}), notice: 'Category was successfully updated.'
+      redirect_to categories_url, notice: 'Category was successfully updated.'
     else
       render :edit
     end
