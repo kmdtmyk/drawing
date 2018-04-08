c6f866c5c3ba1efcd998e13e699ebba84b08857f
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-04-08 21:25:52 +0900

    Add support uploading pdf

--- app/models/drawing_file.rb
+++ app/models/drawing_file.rb
@@ -2,17 +2,27 @@ class DrawingFile < ApplicationRecord
   belongs_to :drawing, optional: true
 
   has_attached_file :file,
-    styles: {
-      # medium: "300x300>", thumb: "100x100>"
+    styles: -> (attachment) {
+      if attachment.instance.pdf?
+        {thumbnail: ['300x300>', :png]}
+      else
+        {}
+      end
     }
+
   validates_attachment_content_type :file,
     content_type: [
       /\Aimage\/.*\z/,
       'text/plain',
+      'application/pdf',
     ]
 
   def image?
     file_content_type.start_with? 'image/'
+  end
+
+  def pdf?
+    file_content_type == 'application/pdf'
   end
 
   def output
