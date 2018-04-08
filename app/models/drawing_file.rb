16117e18b35cb33f15bffce644d53799eae07e32
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-04-08 21:06:07 +0900

    Add uploadable content type text/plain

--- app/models/drawing_file.rb
+++ app/models/drawing_file.rb
@@ -6,6 +6,23 @@ class DrawingFile < ApplicationRecord
       # medium: "300x300>", thumb: "100x100>"
     }
   validates_attachment_content_type :file,
-    content_type: /\Aimage\/.*\z/
+    content_type: [
+      /\Aimage\/.*\z/,
+      'text/plain',
+    ]
+
+  def image?
+    file_content_type.start_with? 'image/'
+  end
+
+  def output
+    ApplicationController.helpers.link_to file.url do
+      if image?
+        ApplicationController.helpers.image_tag file.url
+      else
+        file.original_filename
+      end
+    end
+  end
 
 end
