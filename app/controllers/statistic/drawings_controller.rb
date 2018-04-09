b63a71c7534fec6cf9156cb53aa2cf74e3e00f42
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-04-09 23:45:19 +0900

    Bug fix for drawings statistic

--- app/controllers/statistic/drawings_controller.rb
+++ app/controllers/statistic/drawings_controller.rb
@@ -1,7 +1,10 @@
 class Statistic::DrawingsController < ApplicationController
 
   def index
-    @drawings = Drawing.order(:estimate_date)
+    @drawings = Drawing
+      .where.not(estimate_date: nil)
+      .where.not(customer: nil)
+      .order(:estimate_date)
   end
 
 end
