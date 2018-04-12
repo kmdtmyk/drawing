8d290bf8431388da83847c8dbb7dd11e10686a6a
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-04-13 00:35:16 +0900

    Fix user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
 
   factory :user do
-    sequence(:name) { 'user' }
-    email { 'user@example.com' }
+    name 'user'
+    email 'user@example.com'
     password 'password'
     password_confirmation 'password'
   end
