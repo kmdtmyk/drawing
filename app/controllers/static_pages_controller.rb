28167e005267abc829d9fabd469d906988adcd1a
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-04-22 23:03:43 +0900

    Add limit to top page notices

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def index
-    @notices = Notice.all.order(post_datetime: :desc)
+    @notices = Notice
+      .all
+      .order(post_datetime: :desc)
+      .limit(5)
   end
 
 end
