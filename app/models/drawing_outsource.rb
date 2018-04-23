f3170df6e1c33d672eaf86e1f2b098b3afcc0056
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-04-23 22:47:57 +0900

    Bug fix for no method error in drawings#show

--- app/models/drawing_outsource.rb
+++ app/models/drawing_outsource.rb
@@ -1,4 +1,6 @@
 class DrawingOutsource < ApplicationRecord
   belongs_to :drawing, optional: true
   belongs_to :customer, optional: true
+
+  delegate :name, to: :customer, prefix: true, allow_nil: true
 end
