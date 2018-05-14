200f4187d0f7f11dea46f821bc259c37e7a1a1ee
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-05-14 22:54:19 +0900

    Add backup script

---
+++ script/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd $(dirname ${0})
+cd ..
+
+mkdir -p log
+mkdir -p backup/db
+mkdir -p backup/system
+
+echo tar: start - $(date '+%Y/%m/%d %H:%M:%S')
+tar zcf backup/system/$(date '+%Y%m%d_%H%M').tar.gz public/system
+echo tar: end - $(date '+%Y/%m/%d %H:%M:%S')
+
+echo pg_dump: start - $(date '+%Y/%m/%d %H:%M:%S')
+/usr/local/bin/docker-compose exec -T db pg_dump -U app -v -Fc app_production -f /backup/db/$(date '+%Y%m%d_%H%M').dump
+echo pg_dump: end - $(date '+%Y/%m/%d %H:%M:%S')
