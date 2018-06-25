9da14e1800b4bd3a1982139a02700d58506301f4
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-06-26 00:15:34 +0900

    Create drawings statistics formally

--- app/controllers/statistic/drawings_controller.rb
+++ app/controllers/statistic/drawings_controller.rb
@@ -3,7 +3,6 @@ class Statistic::DrawingsController < ApplicationController
   def index
     @drawings = Drawing
       .where.not(estimate_date: nil)
-      .where.not(customer: nil)
       .order(:estimate_date)
   end
 
