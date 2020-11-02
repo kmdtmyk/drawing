c4c99ee836d186d8c8d37da16ee3c1e19709c4b4
kmdtmyk <winback_banderas@yahoo.co.jp>
2020-11-03 03:32:53 +0900

    トップページ用のコントローラ名を変更

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,6 @@
-class StaticPagesController < ApplicationController
+# frozen_string_literal: true
+
+class RootController < ApplicationController
 
   def index
     @notices = Notice
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#index'
+  root 'root#index'
 
   devise_for :users, controllers: {
     sessions: 'users/sessions'
