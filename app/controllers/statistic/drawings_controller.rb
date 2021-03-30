5d87a0a80fbf0f0622e60a6fa50387aff581ff81
kmdtmyk <winback_banderas@yahoo.co.jp>
2021-03-30 22:40:39 +0900

    統計で日付範囲を選択可能にした

--- app/controllers/statistic/drawings_controller.rb
+++ app/controllers/statistic/drawings_controller.rb
@@ -7,6 +7,15 @@ class Statistic::DrawingsController < ApplicationController
     @drawings = Drawing
       .where.not(estimate_date: nil)
       .order(:estimate_date)
+
+    if params[:from_month].present?
+      @drawings = @drawings.where('? <= estimate_date', params[:from_month])
+    end
+
+    if params[:to_month].present?
+      @drawings = @drawings.where('estimate_date <= ?', params[:to_month].end_of_month)
+    end
+
   end
 
 end
