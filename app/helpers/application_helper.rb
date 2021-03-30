a8dc875b5dbedccd85791be126996f056a24a1a2
kmdtmyk <winback_banderas@yahoo.co.jp>
2021-03-30 23:18:42 +0900

    helperを分割

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,18 +13,6 @@ module ApplicationHelper
     link = link_to *params, &block
   end
 
-  def format_number(number)
-    number_with_delimiter number
-  end
-
-  def format_date(date)
-    date.strftime('%Y/%m/%d') if date.present?
-  end
-
-  def format_datetime(datetime)
-    datetime.strftime('%Y/%m/%d %H:%M:%S') if datetime.present?
-  end
-
   def page_title(title)
     return '図面管理システム' if title.blank?
     title
