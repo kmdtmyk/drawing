3c0bab28db6f5defa5e898a520eed54fbc73b988
kmdtmyk <winback_banderas@yahoo.co.jp>
2021-08-04 14:07:28 +0900

    バックアップを5つまで保持するように修正

--- script/backup.sh
+++ script/backup.sh
@@ -10,5 +10,11 @@ cd ..
 mkdir -p backup/system
 
 echo tar: start - $(date '+%Y/%m/%d %H:%M:%S')
+
 tar zcf backup/system/$(date '+%Y%m%d_%H%M').tar.gz public/system
+
+cd backup/system
+# 新しいの5個だけ残して削除する
+find . -name "*.tar.gz" | sort -r | tail -n +6 | xargs -i rm {}
+
 echo tar: end - $(date '+%Y/%m/%d %H:%M:%S')
