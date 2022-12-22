d3552c619d6c70287ac93d6772cfc422c581b159
kmdtmyk <winback_banderas@yahoo.co.jp>
2022-12-22 21:09:37 +0900

    画像ファイル等のバックアップをまとめてダウンロードできるようにした

--- app/controllers/backup_controller.rb
+++ app/controllers/backup_controller.rb
@@ -3,7 +3,16 @@
 class BackupController < ApplicationController
 
   def index
-    @files = Dir.glob(Rails.root.join('backup', '*.sql'))
+    @dump_files = Dir.glob(Rails.root.join('backup', '*.sql'))
+      .map { |filepath|
+        {
+          size: File.size(filepath),
+          name: File.basename(filepath),
+        }
+      }
+      .sort_by{ |file| file[:name] }
+
+    @pdf_files = Dir.glob(Rails.root.join('backup', 'system', '*.tar.gz'))
       .map { |filepath|
         {
           size: File.size(filepath),
@@ -14,10 +23,16 @@ class BackupController < ApplicationController
   end
 
   def download
-    path = Rails.root.join('backup', "#{params[:filename]}.#{params[:format]}")
+    path = if params[:format] == 'gz'
+      Rails.root.join('backup', 'system', "#{params[:filename]}.tar.#{params[:format]}")
+    else
+      Rails.root.join('backup', "#{params[:filename]}.#{params[:format]}")
+    end
+
     unless File.exists?(path)
       raise ActionController::RoutingError.new('File Not Found')
     end
+
     send_file path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   resources :notices, except: [:show]
 
   get '/backup', to: 'backup#index'
-  get '/backup/:filename', to: 'backup#download'
+  get '/backup/:filename(.tar)', to: 'backup#download'
 
   namespace :admin do
     resources :users, except: [:show]
